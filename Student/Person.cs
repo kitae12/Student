40c1c32b4041078ab01f7ad8e526b848ddd973fd
iampkt <iampkt22@gmail.com>
2021-01-23 17:27:21 +0900

    사람 클래스 추가

---
+++ Student/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Student
+{
+    public class Person
+    {
+        public string name = null;
+        public int age = 0;
+
+        public Person(string name, int age) 
+        {
+            this.name = name;
+            this.age = age;
+
+        }
+    }
+}
--- Student/Program.cs
+++ Student/Program.cs
@@ -6,7 +6,11 @@ namespace Student
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("iampkt!");
+            Person lily = new Person("Lily", 20);
+            Person bob = new Person("Bob", 20);
+
+            Console.WriteLine("Name: " + lily.name + " Age: " + lily.age);
+            Console.WriteLine("Name: " + bob.name + " Age: " + bob.age);
         }
     }
 }
