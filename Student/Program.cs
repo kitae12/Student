1355131c9e0d1f5deaf929293ba1071728676e32
iampkt <iampkt22@gmail.com>
2021-01-23 16:49:20 +0900

    name change

--- Student/Program.cs
+++ Student/Program.cs
@@ -6,7 +6,7 @@ namespace Student
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("iampkt!");
         }
     }
 }
